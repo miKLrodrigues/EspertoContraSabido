297b4c3035fe91e36f9891f51f9f97359eef7d29
Mikaele Rodrigues <mikaelerodrigues007@outlook.com>
2025-11-14 22:43:39 -0300

    Em andamento

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿int parteFink, partePicaPau;
+/*
+
+conta o numero normal
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+conta o numero de forma extensa
+
+1
+1 2
+1 2 3
+1 2 3 4
+
+
+
+*/
+
+
+Console.WriteLine("--- Esperto Contra Sabido ---\n");
+
+Console.WriteLine("Quantos alimentos serão distribuídos? ");
+
+
+
+Console.WriteLine($"Pica-pau recebeu {partePicaPau} alimento(s).");
+Console.WriteLine($"Raposinha recebeu {parteFink} alimento(s).");
+
+
+
+
+
+
+
+
+
+
+
